ada5ecefca33ded6588b33a5f2249cde634961f5
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-03 13:55:50 -0500

    CLASE CABINA: Metodo para mostra la informacion de la cabina

--- Ejercicio01-Avion/src/Modelos/Cabina.java
+++ Ejercicio01-Avion/src/Modelos/Cabina.java
@@ -4,4 +4,35 @@ public class Cabina {
     private int numAsientos;
     private SistemaNavegacion sistemaNavegacion;
     private EquipajeEmergencia equipajeEmergencia;
+
+    public Cabina(int numAsientos, SistemaNavegacion sistemaNavegacion, EquipajeEmergencia equipajeEmergencia) {
+        this.numAsientos = numAsientos;
+        this.sistemaNavegacion = sistemaNavegacion;
+        this.equipajeEmergencia = equipajeEmergencia;
+    }
+
+    public int getNumAsientos() {
+        return numAsientos;
+    }
+
+    public void setNumAsientos(int numAsientos) {
+        this.numAsientos = numAsientos;
+    }
+
+    public SistemaNavegacion getSistemaNavegacion() {
+        return sistemaNavegacion;
+    }
+
+    public void setSistemaNavegacion(SistemaNavegacion sistemaNavegacion) {
+        this.sistemaNavegacion = sistemaNavegacion;
+    }
+
+    public EquipajeEmergencia getEquipajeEmergencia() {
+        return equipajeEmergencia;
+    }
+
+    public void setEquipajeEmergencia(EquipajeEmergencia equipajeEmergencia) {
+        this.equipajeEmergencia = equipajeEmergencia;
+    }
+
 }
