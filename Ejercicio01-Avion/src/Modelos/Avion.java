972b26c0ed45e2e3565b0efaf0805cf171fba6b7
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-03 12:33:11 -0500

    Agregacion de atributos a la clase Avion

--- Ejercicio01-Avion/src/Modelos/Avion.java
+++ Ejercicio01-Avion/src/Modelos/Avion.java
@@ -1,5 +1,14 @@
 package Modelos;
 
 public class Avion {
+    private TipoMotor tipoMotor;
+    private int envergadura;
+    private int lontitud;
+    private int capacidadCarga;
+    private int autonomia;
+    private int altitudMaxima;
+    private SistemaNavegacion sistemaNavegacion;
+    //atributos staticos finales
+    private final int VELOCIDAD_MAXIMA = 1200;
     
 }
---
+++ Ejercicio01-Avion/src/Modelos/SistemaNavegacion.java
@@ -0,0 +1,5 @@
+package Modelos;
+
+public enum SistemaNavegacion {
+    ISTRUMENTOS,GPS,RADAR,VHF,ACARS,ADS_C;
+}
