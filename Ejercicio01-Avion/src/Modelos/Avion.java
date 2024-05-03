54e9d7ba14b2fc4ae39b7f60aca96430c9fe2d7a
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-03 13:44:34 -0500

    Metodo para generar un numero de serie cuando se cree el objeto

--- Ejercicio01-Avion/src/Modelos/Avion.java
+++ Ejercicio01-Avion/src/Modelos/Avion.java
@@ -1,6 +1,9 @@
 package Modelos;
 
+import java.util.Random;
+
 public class Avion {
+    private int numSerie;
     private TipoMotor tipoMotor;
     private int envergadura;
     private int lontitud;
@@ -11,5 +14,25 @@ public class Avion {
     // atributos finales
     private final int VELOCIDAD_MAXIMA = 1200;
     private static final String color = "Blanco";
-    
+
+    // METODO PARA GENERAR UN NUMERO DE SERIE AL AVION CREADO
+    private String generarSerie() {
+        String letras = "QWERTYUIOPASDFGHJKLÑZXCVBNM";
+        String numeros = "1234567890";
+
+        Random random = new Random();
+
+        // generamos 3 letras aleatorias
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append(letras.charAt(random.nextInt(letras.length())));
+        }
+
+        // generamos los 3 numeros aleatorios
+        for (int i = 0; i < 4; i++) {
+            sb.append(numeros.charAt(random.nextInt(numeros.length())));
+        }
+
+        return sb.toString();
+    }
 }
